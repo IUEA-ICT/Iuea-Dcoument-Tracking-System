e317a7e4700cc6fef7d2d3408be5da070e36d454
Jacob.k <jacobkitams200@gmail.com>
2025-05-14 15:06:57 +0300

    Created login page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
